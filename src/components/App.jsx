f8cff3090ed478546c57cd88231a4bd5ccb8dd19
Gowtham Yarlagadda <gowtham777y@gmail.com>
2025-05-26 01:43:10 +0530

    Added functionality to Tenzies Game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,66 @@
 import React from "react"
 import Die from "./Die.jsx"
 import {nanoid} from "nanoid"
+import { useWindowSize } from 'react-use'
+import {useRef, useEffect} from "react"
+import Confetti from "react-confetti"
 
 export default function App() {
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, gameWon);
+
+    const [diceArray, setDiceArray] = React.useState(() => generateAllNewDice());
+
+    var gameWon = diceArray.every(dice => dice.isHeld) && diceArray.every(dice => dice.value === diceArray[0].value);
 
     function generateAllNewDice() {
         return new Array(10)
         .fill(0)
         .map(() => ({
             value : Math.ceil(Math.random()*6),
-            isHeld : true,
+            isHeld : false,
             id : nanoid()
         }));
     }
 
     function hold(id) {
         setDiceArray(prev => prev.map(entry => {
-            if (entry.id === id){
-                return {...entry, isHeld: !entry.isHeld}
-            }
-            return entry;
+            return entry.id === id ? {...entry, isHeld : !entry.isHeld} : entry;
         }))
     }
-
-    const [diceArray, setDiceArray] = React.useState(generateAllNewDice());
 
     const dice = diceArray.map(entry => (
         <Die key = {entry.id} id = {entry.id} value = {entry.value} isHeld = {entry.isHeld} clickHandler = {hold}/>
     ))
 
     function changeDice() {
-        setDiceArray(generateAllNewDice());
+
+        if (gameWon === true){
+            setDiceArray(generateAllNewDice());
+        } else {
+            setDiceArray(prev => prev.map(entry => {
+                if (entry.isHeld === false){
+                    return {...entry, value : Math.ceil(Math.random()*6)}
+                }
+                return entry;
+            }))
+        }
     }
+
+    const {width, height} = useWindowSize();
 
     return (
         <main>
+            {gameWon && <Confetti width={width} height={height} />}
+            <h1>Tenzies</h1>
             <div className="buttonsDiv">
                 {dice}
             </div>
-            <button className="rollDice" onClick={changeDice}>Roll</button>
+            <button ref = {inputRef} className="rollDice" onClick={changeDice}>{gameWon === true ? "New Game" : "Roll"}</button>
         </main>
     )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 
 export default function Die(props){
+
+    const styles = {
+        backgroundColor : props.isHeld ? "#59E391" : "white"
+    }
     
     return (
-        <button style = {props.isHeld && {backgroundColor : "#59E391"}}
+        <button style = {styles} 
         onClick={() => props.clickHandler(props.id)}>{props.value}</button>
     )
 }
